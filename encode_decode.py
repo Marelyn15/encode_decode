938a4aa22ccd453180ade82a95ce0085f6810c3c
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-08-18 03:33:04 -0700

    Update encode_decode.py

--- encode_decode.py
+++ encode_decode.py
@@ -4,44 +4,27 @@ while answer == 0:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
+    def ceasars_encode_decode(original_text,shift_amount, encode_decode):
 
-    if direction == 'encode':
-        def encrypt(original_text, shift_amount):
-            new_text_list = []
-            for letter in original_text:
-                new_letter = alphabet.index(letter) + shift_amount
-                #Always in the range 0, 25. What happens if you try to shift z forwards by 9? Can you fix the code?
-                new_letter %= len(alphabet)
-                new_text_list.append(new_letter)
-            print(new_text_list)
+        if encode_decode == 'decode':
+            shift_amount = shift_amount * -1
 
-            new_text = ''
-            for element in new_text_list:
-                new_text = new_text + alphabet[element]
+        
+        new_text_list = []
+        for letter in original_text:
+            new_letter = alphabet.index(letter) + shift_amount
+            #Always in the range 0, 25. What happens if you try to shift z forwards by 9? Can you fix the code?
+            new_letter %= len(alphabet)
+            new_text_list.append(new_letter)
 
-            print(f"Here's the encode result: {new_text}")
-            
-        encrypt(text, shift)
+        new_text = ''
+        for element in new_text_list:
+            new_text = new_text + alphabet[element]
 
-    elif direction == 'decode':
-        def decrypt(original_text, shift_amount):
-            new_text_list = []
-            for letter in original_text:
-                new_letter = alphabet.index(letter) - shift_amount
-                # Always in the range 0, 25
-                new_letter %= len(alphabet)
-                new_text_list.append(new_letter)
+        print(f"Here's the {encode_decode} result: {new_text}")
+                
 
-            new_text = ''
-            for element in new_text_list:
-                new_text = new_text + alphabet[element]
-
-            print(f"Here's the decode result: {new_text}")
-        decrypt(text, shift)
-
-    else:
-        print("You must put the right word: encode or decode.")
-
+    ceasars_encode_decode(original_text = text, shift_amount= shift, encode_decode=direction)
 
     answer = input("Type 'yes' if you want to go again, if not type 'no'. :P \n").lower()
     if answer == "yes" or answer == "y":
